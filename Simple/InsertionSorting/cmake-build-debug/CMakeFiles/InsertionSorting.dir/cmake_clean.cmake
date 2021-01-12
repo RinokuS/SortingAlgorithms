5df07cf55851b781dccfccd59783843200cce097
RinokuS <JeffTheKilller@mail.ru>
2021-01-12 13:57:10 +0300

    Added insertion sorting implementation

---
+++ Simple/InsertionSorting/cmake-build-debug/CMakeFiles/InsertionSorting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InsertionSorting.dir/main.cpp.o"
+  "InsertionSorting"
+  "InsertionSorting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InsertionSorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Simple/InsertionSorting/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void insertion_sorting(vector<T>& vec) {
+    size_t ind_for_insertion;
+    for (size_t i = 1; i < vec.size(); ++i) {
+        ind_for_insertion = i;
+        T temp = vec[i];
+        vec.erase(vec.begin() + i);
+
+        for (size_t j = i; j > 0; --j)
+            if (vec[j - 1] > temp)
+                ind_for_insertion = j - 1;
+
+        vec.insert(vec.begin() + ind_for_insertion, temp);
+    }
+}
+
+int main() {
+    vector<int> test = {64, 34, 25, 12, 22, 11, 90, 1, 11};
+    insertion_sorting(test);
+
+    for (const auto& elem: test)
+        cout << elem << ' ';
+
+    return 0;
+}
